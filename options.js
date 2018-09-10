002e454abcab7b2a75358eb370e8d4c545adcd28
Anna <annagassen@me.com>
2018-09-10 13:17:31 -0700

    Breed Options

--- options.js
+++ options.js
@@ -1,9 +1,34 @@
 let dropdown = document.getElementById('dropdown');
+dropdown.length = 0;
+dropdown.selectedIndex = 0;
+
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://dog.ceo/api/breeds/list/all', true);
+
+request.onload = function() {
+	if (request.status >= 200 && request.status < 400) {
+		var data = JSON.parse(this.response);
+		let option;
+		for (x in data.message) {
+			option = document.createElement('option');
+			option.text = x;
+			option.value = x;
+			dropdown.add(option);
+		}
+	}
+}
+
+request.onerror = function() {
+	console.error('An error occurred fetching the JSON');
+};
+
+request.send();
+
 let submitButton = document.getElementById('submitButton');
 
 submitButton.addEventListener('click', function() {
 	chrome.storage.sync.set({breed: dropdown.options[dropdown.selectedIndex].text}, function() {
-		console.log('breed is' + dropdown.options[dropdown.selectedIndex].text);
+		console.log('breed is ' + dropdown.options[dropdown.selectedIndex].text);
 	});
 });
-
--- popup.js
+++ popup.js
@@ -6,21 +6,20 @@ chrome.storage.sync.get('breed', function(data) {
 	var dogBreed = 'https://dog.ceo/api/breed/' + data.breed + '/images/random';
 	request.open('GET', dogBreed, true);
 
-request.onload = function() {
-	var data = JSON.parse(this.response);
+	request.onload = function() {
+		var data = JSON.parse(this.response);
 
-	if (request.status >= 200 && request.status < 400) {
-		puppyPhoto.src = data.message;
-		$(function() {
-			$("img.scale").imageScale();
-		});
+		if (request.status >= 200 && request.status < 400) {
+			puppyPhoto.src = data.message;
+			$(function() {
+				$("img.scale").imageScale();
+			});
+		}
+		else {
+			console.log('error');
+		}
 	}
-	else {
-		console.log('error');
-	}
-}
 
-request.send();
-
+	request.send();
 });
 
